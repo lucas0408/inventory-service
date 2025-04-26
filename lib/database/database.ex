f4d2ccef67a39082c3a84893037c1a829fe1c279
root <root@lucas0408.>
2025-04-26 01:57:17 -0300

    feature: adding buy_product feature

---
+++ lib/database/database.ex
@@ -0,0 +1,56 @@
+defmodule InventoryService.Database do
+    use GenServer
+
+    def start_link(table_name) do
+        GenServer.start(__MODULE__, table_name, name: __MODULE__)
+    end
+
+    def create(product, product_id) do
+        product_id
+        |> chose_worker()
+        |> InventoryService.DatabaseWorker.create(product)
+    end
+
+    
+    def get_all(random) do
+        random
+        |> chose_worker()
+        |> InventoryService.DatabaseWorker.get_all()
+    end
+
+    def update(update_product, product_id) do
+        product_id
+        |> chose_worker()
+        |> InventoryService.DatabaseWorker.update(update_product, product_id)
+    end
+
+    def delete(product_id) do
+        product_id
+        |> chose_worker()
+        |> InventoryService.DatabaseWorker.delete(product_id)
+    end
+
+    defp chose_worker(market_id) do
+        GenServer.call(__MODULE__, {:chose_worker, market_id})
+    end
+
+    @impl GenServer
+    def init(table_name) do
+        IO.inspect(table_name)
+        {:ok, gen_workers(table_name)}
+    end
+
+    @impl GenServer
+    def handle_call({:chose_worker, market_id}, _from, workers) do
+        index = :erlang.phash2(market_id, 3)
+        {:reply, Map.get(workers, index), workers}
+    end
+
+
+    defp gen_workers(table_name) do
+        for index <- 1..3, into: %{} do
+            {:ok, pid} = InventoryService.DatabaseWorker.start(table_name)
+            {index - 1, pid}
+        end
+    end
+end
--- lib/stock_control/stock.ex
+++ lib/stock_control/stock.ex
@@ -44,8 +44,18 @@ defmodule InventoryService.Stock do
   @impl GenServer
   def handle_cast({:update_product, product_id, update_product}, {name, stock}) do
     updated_products = Map.replace(stock.products, product_id, update_product)
-    new_state = %Market.Stock{stock | products: updated_products}
-    Market.Database.store(name, new_state)
+    new_state = %InventoryService.Stock{stock | products: updated_products}
+
+    product_key_atom = for {key, val} <- update_product, into: %{} do
+      if is_binary(key) do
+        {String.to_atom(key), val}
+      else
+        {key, val}
+      end
+    end
+
+    InventoryService.Database.update(product_key_atom, product_id)
+>>>>>>> Stashed changes
     {
       :noreply,
       {name, new_state}
@@ -64,7 +74,19 @@ defmodule InventoryService.Stock do
   end
 
   @impl GenServer
-  def handle_call({:get_product, product_id}, _from, {name, stock}) do
+  def handle_cast({:buy_product, product_id, quantity}, stock) do
+    product = Map.get(stock.products, product_id)
+    case product.quantity do
+      0 ->
+        InventoryService.RabbitMQProducer.publish("erro ao efetuar compra, produto esgotado")
+
+      product_quantity when product_quantity < quantity ->
+        InventoryService.RabbitMQProducer.publish("erro ao efetuar compra, quantidade insuficiente")
+
+      _ ->
+        update_product = product.quantity - quantity
+        GenServer.cast(self(), {:update_product, product_id, update_product})
+    end 
     {
       :reply,
       Map.get(stock.products, product_id),
@@ -73,8 +95,12 @@ defmodule InventoryService.Stock do
   end
 
   @impl GenServer
-  def handle_call({:get_database, message}, _stock) do
-   {:reply, message, {market_name, Market.Database.get(market_name) || %Market.Stock{}}}
+  def handle_cast({:get_all}, stock) do
+    InventoryService.RabbitMQProducer.publish(message.meta, stock.products)
+    {
+      :noreply,
+      stock
+    }
   end
 
   @impl GenServer
@@ -86,11 +112,12 @@ defmodule InventoryService.Stock do
       "delete" ->
         GenServer.cast(self(), {:delete_product, message["product_id"]})
       "add" ->
-        GenServer.call(self(), {:add_product, message["product"]})
-      "get" ->
-        GenServer.call(self(), {:get_product, message["product_id"]})
+        product_id = GenServer.call(self(), {:add_product, message["product"]})
+        send(InventoryService.Cache, {:product_created, self(), product_id})
+      "buy_product" ->
+        GenServer.cast(self(), {:buy_product, message["product_id"], message["quantity"]})
       "get_all" ->
-
+        GenServer.cast(self(), {:get_all, message})
     end
     {:noreply, {market_name, Market.Database.get(market_name) || %Market.Stock{}}}
   end
