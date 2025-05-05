2046540d981be926289f019869422a323b879cd2
root <root@lucas0408.>
2025-05-05 20:27:58 -0300

    feat: add init_supervisor

---
+++ lib/supervisor/init_supervisor.ex
@@ -0,0 +1,44 @@
+defmodule InventoryService.InitSupervisor do
+  use Supervisor
+
+  def start_link(_) do
+    Supervisor.start_link(__MODULE__, nil)
+  end
+
+  defp poolboy_config do
+    [
+      name: {:local, :worker_stock},
+      worker_module: InventoryService.Stock,
+      size: 10,
+      max_overflow: 10,
+      worker_restart_strategy: :one_for_one,
+      max_restart_intensity: 3,
+      max_restart_period: 5
+    ]
+  end
+
+  def init(_) do
+  
+    children = [
+        %{
+          id: InventoryService.Database,
+          start: {InventoryService.Database, :start_link, ["stocks"]},
+          restart: :permanent,
+          shutdown: 5000,
+          type: :supervisor,
+          modules: [InventoryService.DatabaseWorker]
+        },
+        %{
+          id: InventoryService.RabbitMQConfig,
+          start: {InventoryService.RabbitMQConfig, :start_link, []},
+          restart: :permanent,
+          shutdown: 5000,
+          type: :supervisor,
+          modules: [InventoryService.RabbitMQConfig]
+        },
+      :poolboy.child_spec(:worker_stock, poolboy_config())
+    ]
+    
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
